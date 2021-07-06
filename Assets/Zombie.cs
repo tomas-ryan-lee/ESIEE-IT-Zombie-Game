42ff49176c31c71a98d0aad618ce3dcbdbc5d5d7
Deschaepmeester Samantha <deschaepmeester.samantha@gmail.com>
2021-07-06 14:12:37 +0200

    Zombie avoid walls

--- Assets/Zombie.cs
+++ Assets/Zombie.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
-    // Variables
     public Transform target;
     public float speed = 4f;
+    public NavMeshAgent agent; 
     Rigidbody rig;
-    void Start() {
+    void Awake() {
         rig=GetComponent<Rigidbody>();
     }
 
     void FixedUpdate() {
-        // Make the ennemy moves toward the player
+        // Make the zombie moves toward the player
         Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed*Time.fixedDeltaTime);
-        rig.MovePosition(pos);
-        transform.LookAt(target);
+        /*rig.MovePosition(pos);
+        transform.LookAt(target); */
+        agent.SetDestination(pos);
         
     }
 }
